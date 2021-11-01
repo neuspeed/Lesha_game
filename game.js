63a10aaee740a14a6afa8c6394ded522ea9bba8f
Михаил Ляхов <->
2021-11-01 17:25:29 +0400

    v0.3

--- game.js
+++ game.js
@@ -1,15 +1,50 @@
+const PLATFORM_HEIGHT = 150
+const SPEED = 150
+const PSPEED = 1000
+
+var projectileSpawned = false
 kaboom({
+    width: 1280,
+    height: 720,
     global: true,
-    fullscreen: true,
     clearColor: [0, 0, 0, 1],
 })
 
-loadSprite("batman", "sprites/batman.png")
-loadSprite("batarang", "sprites/batarang.png")
+loadRoot("sprites/")
+loadSprite("batman", "batman.png")
+loadSprite("batarang", "batarang.png")
+loadSprite("background", "background.png")
+loadSprite("badguy", "badguy.png")
+
+
+// custom comp
+function handleout() {
+	return {
+		id: "handleout",
+		require: [ "pos" ],
+		update() {
+			const spos = this.screenPos();
+			if (
+				spos.x < 0 ||
+				spos.x > width() ||
+				spos.y < 0 ||
+				spos.y > height()
+			) {
+				// triggers a custom event when out
+				this.trigger("out");
+			}
+		},
+	};
+}
+
+const backgorund = add([
+    sprite("background"),
+    pos(0, 0),
+])
 
 const player = add([
     sprite("batman"),
-    pos(50, 200),
+    pos(0, 0),
     scale(0.2),
     area(),
     body(),
@@ -18,12 +53,11 @@ const player = add([
 
 
 
-const SPEED = 150
-const PSPEED = 1000
+
 
 keyPress("space", () => {
     if (player.grounded())
-    player.jump()
+    player.jump(1000)
 })
 
 keyDown("right", () => {
@@ -34,31 +68,47 @@ keyDown("left", () => {
     player.move(-SPEED, 0);
 })
 
-keyPress("q", () => {
-    add([
-        sprite("batarang"),
-        pos(player.pos.x + 10, player.pos.y),
-        scale(0.02),
-        area(),
-        move((0,15), PSPEED),
-        "projectile",
-    ])
+keyDown("d", () => {
+    player.move(SPEED, 0);
 })
+
+keyDown("a", () => {
+    player.move(-SPEED, 0);
+})
+
+keyPress("q", () => {
+    if (!projectileSpawned) {
+        var batarang = add([
+            sprite("batarang"),
+            pos(player.pos),
+            scale(0.02),
+            area(),
+            move((0,15), PSPEED),
+            "projectile",
+            handleout(),
+        ])
+    
+        batarang.action(() => {
+            batarang.angle = time() * 500;
+        })
+        projectileSpawned = true
+    }
+})
+
+
 
 function spawnTree() {
     // add tree
     add([
-        rect(48, 64),
+        sprite("badguy"),
+        scale(0.2),
         area(),
-        outline(4),
-        pos(width(), height() - 48),
+        pos(width(), height() - PLATFORM_HEIGHT),
         origin("botleft"),
-        color(255, 180, 255),
         move(LEFT, 240),
-        rotate(5),
         "obstacle",
     ]);
-    wait(rand(0.5, 1.5), () => {
+    wait(rand(1.5, 3.0), () => {
         spawnTree();
     });
 }
@@ -66,22 +116,33 @@ function spawnTree() {
 spawnTree();
 
 // add platform
-add([
-    rect(width(), 48),
-    pos(0, height() - 48),
-    outline(4),
+const platform = add([
+    rect(width(), PLATFORM_HEIGHT),
+    pos(0, height() - PLATFORM_HEIGHT),
     area(),
     solid(),
-    color(50, 50, 50),
+    opacity(0.0),
+    "platform",
 ])
 
 collides("projectile", "obstacle", (b, a) => {
     if (!a.initializing) {
+        addKaboom(b.pos);
         destroy(b);
         destroy(a);
+        projectileSpawned = false;
     }
 });
 
+collides("projectile", "platform", (a) => {
+    destroy(a);
+    projectileSpawned = false;
+})
+
+on("out", "projectile", (m) => {
+    destroy(m);
+    projectileSpawned = false;
+})
 // scene("game", () => {
 
 // })
